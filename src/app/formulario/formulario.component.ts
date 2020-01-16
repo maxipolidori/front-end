d05f74dd47ae2249af605621a4d4f3272da35edc
maxipolidori <maxipolidori@gmail.com>
2020-01-16 17:24:38 -0300

    Ahora obtiene id del cliente y lo muestra

--- src/app/formulario/formulario.component.ts
+++ src/app/formulario/formulario.component.ts
@@ -1,29 +1,38 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder } from '@angular/forms';
-import {ClientesService} from '../clientes.service';
+import { Validators, ValidatorFn, FormBuilder, FormControl, FormGroup } from '@angular/forms';
+import { ClientesService} from '../clientes.service';
+import { Router } from '@angular/router';
+import { Cliente } from '../cliente';
+import { FormInput } from '../form-input';
+import { FormService } from '../form.service';
 
 @Component({
   selector: 'app-formulario',
   templateUrl: './formulario.component.html',
   styleUrls: ['./formulario.component.css']
 })
-  export class FormularioComponent {
+  export class FormularioComponent implements OnInit {
 
-  checkoutForm;
+  form: FormGroup;
+  inputs: FormInput[];
 
   constructor(
-    private formBuilder: FormBuilder,
-    private clienteService: ClientesService
-  ) {
-    this.checkoutForm = this.formBuilder.group(
-      {
-    nombre: '',
-    apellido: '',
-    direccion: ''
-  });
+    private clienteService: ClientesService,
+    private router: Router,
+    private fs: FormService
+  ) {}
+
+  
+  ngOnInit()
+  {
+    this.inputs = this.fs.getInputs();
+    this.form = this.fs.getForm();
   }
 
-  onSubmit(clientData) {
-    this.clienteService.addCliente(clientData);
+  public async onSubmit(clientData: Cliente): Promise<void>
+  {
+    let cl = await this.clienteService.addCliente(clientData);
+    window.alert(`Cliente creado con ID: ${cl.id}`);
+    this.router.navigate(['/clientes']);
   }
 }
