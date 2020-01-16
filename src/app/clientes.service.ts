10a7b7ae2b039cdf8f46c1220f7b1f794037f0d7
maxipolidori <maxipolidori@gmail.com>
2020-01-16 16:12:40 -0300

    Agregados métodos para actualizar, eliminar y obtener cliente por id

--- src/app/clientes.service.ts
+++ src/app/clientes.service.ts
@@ -1,36 +1,53 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import { Router } from '@angular/router';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Cliente } from './cliente';
 
 
-/*const httpOptions = {
+const httpOptions = {
   headers: new HttpHeaders({
     'Content-Type':  'application/json',
     responseType: 'text'
   })
-};*/
+};
 
 @Injectable()
 export class ClientesService {
 
+  serverBaseUrl: string;
+
   constructor(
-    private http: HttpClient,
-    private router: Router,
-  ) {}
+    private http: HttpClient
+  ) 
+  {
+    this.serverBaseUrl = 'http://localhost:5000'
+  }
 
-  addCliente(cliente) {
-    this.http.post('http://localhost:5000/', cliente).
-    toPromise().then(res => 
-      {
-        this.router.navigate(['/clientes']);
-      });
-
-    }
+  public async addCliente(cliente: Cliente): Promise<Cliente>
+  {
+    let cl = await this.http.post<Cliente>(this.serverBaseUrl, cliente, httpOptions).toPromise();
+    return cl;   
+  }
 
   public async getClientes(): Promise<Array<Cliente>>
   {
-    let clientes = await this.http.get<Array<Cliente>>('http://localhost:5000/clientes').toPromise();
+    let clientes = await this.http.get<Array<Cliente>>(this.serverBaseUrl + '/clientes', httpOptions).toPromise();
     return clientes;
+  }
+
+  public async getCliente(id:number): Promise<Cliente>
+  {
+    let cliente = await this.http.get<Cliente>(`${this.serverBaseUrl}/clientes/${id}`, httpOptions).toPromise();
+    return cliente;
+  }
+
+  public async updateCliente(cliente: Cliente): Promise<Cliente>
+  {
+    let c = await this.http.put<Cliente>(`${this.serverBaseUrl}/clientes/${cliente.id}`, cliente, httpOptions).toPromise();
+    return c;
+  }
+
+  public async removeCliente(id: number): Promise<void>
+  {
+    let resp = await this.http.delete<Cliente>(`${this.serverBaseUrl}/clientes/${id}`, {observe: 'response'}).toPromise();
   }
 }
