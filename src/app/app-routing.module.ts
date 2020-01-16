e1259fb491d3e4f638da85e74e242052eb527913
maxipolidori <maxipolidori@gmail.com>
2020-01-16 16:14:25 -0300

    Rutas agregadas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,21 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { FormularioComponent } from './formulario/formulario.component';
+import { ViewClientesComponent } from './view-clientes/view-clientes.component';
+import { MainComponent } from './main/main.component';
+import { InfoclienteComponent } from './infocliente/infocliente.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: MainComponent },
+  { path: 'add', component: FormularioComponent },
+  { path: 'clientes', component: ViewClientesComponent },
+  { path: 'clientes/:id', component: InfoclienteComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
+
 export class AppRoutingModule { }
