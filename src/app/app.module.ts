eb813e49cf7504208f689decfd6d0138445b997c
maxipolidori <maxipolidori@gmail.com>
2020-01-16 16:13:38 -0300

    -

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,28 +1,30 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { RouterModule } from '@angular/router';
-import { ReactiveFormsModule } from '@angular/forms';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
-
 import { AppComponent } from './app.component';
 import { FormularioComponent } from './formulario/formulario.component';
 import { ViewClientesComponent } from './view-clientes/view-clientes.component';
 import { ClientesService } from './clientes.service';
+import { AppRoutingModule } from './app-routing.module';
+import { MainComponent } from './main/main.component';
+import { InfoclienteComponent } from './infocliente/infocliente.component';
 
 @NgModule({
   imports: [
     BrowserModule,
     ReactiveFormsModule,
     HttpClientModule,
-    RouterModule.forRoot([
-      { path: '', component: FormularioComponent },
-      { path: 'clientes', component: ViewClientesComponent },
-    ])
+    AppRoutingModule,
+    ReactiveFormsModule,
+    FormsModule
   ],
   declarations: [
     AppComponent,
     FormularioComponent,
-    ViewClientesComponent
+    ViewClientesComponent,
+    MainComponent,
+    InfoclienteComponent,
   ],
   bootstrap: [ AppComponent ],
   providers: [ClientesService],
